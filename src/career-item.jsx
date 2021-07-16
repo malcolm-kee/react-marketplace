df0ed691c69dd73c486f7e0edc1da0cad1016e48
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-17 01:00:19 +0800

    reuse CareerItem and add condition

--- src/career-item.jsx
+++ src/career-item.jsx
@@ -87,6 +87,11 @@ export function CareerItem(props) {
               <div className="flex items-center gap-2 text-sm leading-5 text-gray-500">
                 <WorkingBagIcon />
                 <span>Level: {props.level} </span>
+                {props.studentFriendly && (
+                  <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+                    Student-friendly
+                  </span>
+                )}
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import * as ReactDOM from "react-dom";
 import { CareerItem } from "./career-item";
 
 ReactDOM.render(
-  <div className="max-w-4xl mx-auto py-12">
+  <div className="max-w-4xl mx-auto py-12 space-y-5">
     <CareerItem
       title="Memer"
       department="UI / UX Design"
       level="Experienced"
       onEdit={() => alert("Edit btn clicked, populate the form!")}
       onDelete={() => alert("Delete btn clicked, delete the item!")}
+    />
+    <CareerItem
+      title="GitHub Issue Commentor"
+      department="Engineering"
+      level="Internship"
+      onEdit={() => alert("Edit btn clicked, populate the form!")}
+      onDelete={() => alert("Delete btn clicked, delete the item!")}
+      studentFriendly
     />
   </div>,
   document.getElementById("root")
