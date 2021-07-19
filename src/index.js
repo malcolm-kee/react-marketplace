9ed173f40849fa4b7b7ee68c635d9ec68f0b2f52
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-19 17:40:29 +0800

    show faq components

--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { CareerItem } from "./career-item";
 import { jobs } from "./data";
+import { Faq } from "./pages/faq";
 
 ReactDOM.render(
   <div className="max-w-4xl mx-auto py-12 space-y-5">
-    {jobs.map((job) => (
-      <CareerItem
-        title={job.title}
-        department={job.department}
-        level={job.level}
-        onEdit={() => alert("Edit btn clicked, populate the form!")}
-        onDelete={() => alert("Delete btn clicked, delete the item!")}
-        studentFriendly={job.level === "internship"}
-        key={job._id}
-      />
-    ))}
+    <Faq />
   </div>,
   document.getElementById("root")
 );
