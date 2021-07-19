2b5cfc9578f8f07879cfaaef7df1404f10457e36
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-19 17:40:19 +0800

    add accordion and faq components

---
+++ src/pages/faq.jsx
@@ -0,0 +1,32 @@
+import { Accordion } from "../components/accordion";
+
+export const Faq = () => {
+  return (
+    <div className="space-y-6">
+      <Accordion title="What's the best thing about Switzerland?">
+        <p className="text-base text-gray-500">
+          I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.
+        </p>
+      </Accordion>
+      <Accordion title="How can I get refund?">
+        <p className="text-base text-gray-500">
+          We do not give refund, so try at your risk.
+        </p>
+        <p className="text-base text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+          facere hic sed nemo porro error libero corrupti voluptas assumenda
+          nesciunt fugiat rem, aut, rerum quaerat eum accusamus adipisci. Culpa,
+          modi amet est nobis odio eveniet! Quisquam ipsum alias molestiae
+          ipsam.
+        </p>
+      </Accordion>
+      <Accordion title="Can I become a millionaire by learning JavaScript?">
+        <p className="text-base text-gray-500">
+          The only thing that can surely make you a millionaire is by having one
+          as your parents. Everything else can't ensure that.
+        </p>
+      </Accordion>
+    </div>
+  );
+};
