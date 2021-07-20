4f53e09ba69756bdff0dc42cf0b9913e9b7560b5
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-20 15:34:49 +0800

    add NumberControl component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { ListingItem } from "./listing-item";
-import { listings } from "./data";
+import { NumberControl } from "./components/number-control";
 
-ReactDOM.render(
-  <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
-    {listings.map((listing) => (
-      <ListingItem
-        imageUrl={listing.imageUrl}
-        title={listing.title}
-        description={listing.description}
-        price={listing.price}
-        availableStock={listing.numOfStock}
-        onlyOne={listing.availability === "single-item"}
-        key={listing._id}
-      />
-    ))}
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<NumberControl />, document.getElementById("root"));
