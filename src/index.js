f360b6a50010d120f2b69db22b3892fbde39fb93
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-20 13:36:03 +0800

    adding props and decompose into smaller components

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ListingItem } from "./listing-item";
 
-ReactDOM.render(<ListingItem />, document.getElementById("root"));
+ReactDOM.render(
+  <ListingItem
+    imageUrl="https://images.unsplash.com/photo-1594995846645-d58328c3ffa4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=543&h=384&q=80"
+    title="Classic Quartz Clock"
+    description="Inexpensive. Best as birthday gift for your enemy."
+    price="10"
+    availableStock="143"
+  />,
+  document.getElementById("root")
+);
