860ad4c49843e9d588e4278aa08d7bbfac3d97c7
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-25 02:41:19 +0800

    add button and clicked state

--- src/pages/career.jsx
+++ src/pages/career.jsx
@@ -1,7 +1,11 @@
 import { jobs } from "../data";
 import { CareerItem } from "../components/career-item";
+import { Button } from "../components/button";
+import * as React from "react";
 
 export const Careers = () => {
+  const [clicked, setClicked] = React.useState(false);
+
   return (
     <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
       <div className="mb-8">
@@ -19,6 +23,15 @@ export const Careers = () => {
             key={job._id}
           />
         ))}
+      </div>
+      <div className="text-center">
+        <Button
+          onClick={() => {
+            setClicked(true);
+          }}
+        >
+          {clicked ? "Loading..." : "Load"}
+        </Button>
       </div>
     </div>
   );
