426e503e441e560a62c20a7a1f82f3b60b0e54e9
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-25 02:53:11 +0800

    tweak marketplace style

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -4,18 +4,25 @@ import { listings } from "../data";
 
 export const Marketplace = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
-      {listings.map((listing) => (
-        <ListingItem
-          imageUrl={listing.imageUrl}
-          title={listing.title}
-          description={listing.description}
-          price={listing.price}
-          availableStock={listing.numOfStock}
-          onlyOne={listing.availability === "single-item"}
-          key={listing._id}
-        />
-      ))}
+    <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
+      <div className="mb-8">
+        <div>
+          <h1 className="text-6xl mb-4 font-extrabold">Marketplace</h1>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
+        {listings.map((listing) => (
+          <ListingItem
+            imageUrl={listing.imageUrl}
+            title={listing.title}
+            description={listing.description}
+            price={listing.price}
+            availableStock={listing.numOfStock}
+            onlyOne={listing.availability === "single-item"}
+            key={listing._id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
