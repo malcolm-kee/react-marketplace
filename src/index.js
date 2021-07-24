ba87bfd55c7ba85507bb6820e76edfb83f70aad3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-25 02:57:50 +0800

    demo how to display listings from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Careers } from "./pages/career";
+import { Marketplace } from "./pages/marketplace";
 
-ReactDOM.render(<Careers />, document.getElementById("root"));
+ReactDOM.render(<Marketplace />, document.getElementById("root"));
--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { ListingItem } from "../components/listing-item";
-import { listings } from "../data";
+import { Button } from "../components/button";
+
+const getListings = () =>
+  fetch("https://ecomm-service.herokuapp.com/marketplace").then((res) =>
+    res.json()
+  );
 
 export const Marketplace = () => {
+  const [clicked, setClicked] = React.useState(false);
+  const [listings, setListings] = React.useState(undefined);
+
   return (
     <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
       <div className="mb-8">
@@ -11,18 +19,31 @@ export const Marketplace = () => {
         </div>
       </div>
       <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
-        {listings.map((listing) => (
-          <ListingItem
-            imageUrl={listing.imageUrl}
-            title={listing.title}
-            description={listing.description}
-            price={listing.price}
-            availableStock={listing.numOfStock}
-            onlyOne={listing.availability === "single-item"}
-            key={listing._id}
-          />
-        ))}
+        {listings &&
+          listings.map((listing) => (
+            <ListingItem
+              imageUrl={listing.imageUrl}
+              title={listing.title}
+              description={listing.description}
+              price={listing.price}
+              availableStock={listing.numOfStock}
+              onlyOne={listing.availability === "single-item"}
+              key={listing._id}
+            />
+          ))}
       </div>
+      {!listings && (
+        <div className="text-center">
+          <Button
+            onClick={() => {
+              setClicked(true);
+              getListings().then((result) => setListings(result));
+            }}
+          >
+            {clicked ? "Loading..." : "Load Listing"}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
