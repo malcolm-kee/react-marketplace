ce66e09d538fb260aa448208be8d4d5bcb519300
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-25 02:43:54 +0800

    load jobs from api

--- src/pages/career.jsx
+++ src/pages/career.jsx
@@ -1,10 +1,15 @@
-import { jobs } from "../data";
 import { CareerItem } from "../components/career-item";
 import { Button } from "../components/button";
 import * as React from "react";
 
+const getJobs = () =>
+  fetch("https://ecomm-service.herokuapp.com/job?limit=5").then((res) =>
+    res.json()
+  );
+
 export const Careers = () => {
   const [clicked, setClicked] = React.useState(false);
+  const [jobs, setJobs] = React.useState(undefined);
 
   return (
     <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
@@ -14,25 +19,29 @@ export const Careers = () => {
         </div>
       </div>
       <div className="space-y-3">
-        {jobs.map((job) => (
-          <CareerItem
-            title={job.title}
-            department={job.department}
-            level={job.level}
-            studentFriendly={job.level === "internship"}
-            key={job._id}
-          />
-        ))}
+        {jobs &&
+          jobs.map((job) => (
+            <CareerItem
+              title={job.title}
+              department={job.department}
+              level={job.level}
+              studentFriendly={job.level === "internship"}
+              key={job._id}
+            />
+          ))}
       </div>
-      <div className="text-center">
-        <Button
-          onClick={() => {
-            setClicked(true);
-          }}
-        >
-          {clicked ? "Loading..." : "Load"}
-        </Button>
-      </div>
+      {!jobs && (
+        <div className="text-center">
+          <Button
+            onClick={() => {
+              setClicked(true);
+              getJobs().then((result) => setJobs(result));
+            }}
+          >
+            {clicked ? "Loading..." : "Load"}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
