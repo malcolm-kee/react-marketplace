fc391c2bacb6e886800f5fafc3403b6033ba0515
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-25 11:15:53 +0800

    tweak marketplace layout

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -12,38 +12,42 @@ export const Marketplace = () => {
   const [listings, setListings] = React.useState(undefined);
 
   return (
-    <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
-      <div className="mb-8">
-        <div>
-          <h1 className="text-6xl mb-4 font-extrabold">Marketplace</h1>
+    <div className="lg:flex bg-gray-50">
+      <div className="flex-1">
+        <div className="max-w-6xl mx-auto px-3 py-12 space-y-6">
+          <div className="mb-8">
+            <div>
+              <h1 className="text-6xl mb-4 font-extrabold">Marketplace</h1>
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
+            {listings &&
+              listings.map((listing) => (
+                <ListingItem
+                  imageUrl={listing.imageUrl}
+                  title={listing.title}
+                  description={listing.description}
+                  price={listing.price}
+                  availableStock={listing.numOfStock}
+                  onlyOne={listing.availability === "single-item"}
+                  key={listing._id}
+                />
+              ))}
+          </div>
+          {!listings && (
+            <div className="text-center">
+              <Button
+                onClick={() => {
+                  setClicked(true);
+                  getListings().then((result) => setListings(result));
+                }}
+              >
+                {clicked ? "Loading..." : "Load Listing"}
+              </Button>
+            </div>
+          )}
         </div>
       </div>
-      <div className="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
-        {listings &&
-          listings.map((listing) => (
-            <ListingItem
-              imageUrl={listing.imageUrl}
-              title={listing.title}
-              description={listing.description}
-              price={listing.price}
-              availableStock={listing.numOfStock}
-              onlyOne={listing.availability === "single-item"}
-              key={listing._id}
-            />
-          ))}
-      </div>
-      {!listings && (
-        <div className="text-center">
-          <Button
-            onClick={() => {
-              setClicked(true);
-              getListings().then((result) => setListings(result));
-            }}
-          >
-            {clicked ? "Loading..." : "Load Listing"}
-          </Button>
-        </div>
-      )}
     </div>
   );
 };
