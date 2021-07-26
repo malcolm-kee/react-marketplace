0f702be22ba44e59f9a7a077a9b603cbdc172e58
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-27 00:31:04 +0800

    add value state

--- src/components/number-control.jsx
+++ src/components/number-control.jsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 
 export const NumberControl = () => {
+  const [value, setValue] = React.useState(1);
+
   return (
     <div className="mt-1 sm:mt-0 sm:col-span-2">
       <div className="relative w-32">
         <button
           type="button"
           className="absolute left-0 inset-y-0 px-1.5 text-gray-400"
+          onClick={() => setValue(value - 1)}
         >
           <svg
             className="w-6 h-6"
@@ -28,10 +31,12 @@ export const NumberControl = () => {
           required
           className="block w-full px-9 text-center shadow-sm sm:text-sm focus:ring-pink-500 focus:border-pink-500 border-gray-300 rounded-md"
           readOnly
+          value={value}
         />
         <button
           type="button"
           className="absolute right-0 inset-y-0 px-1.5 text-gray-400"
+          onClick={() => setValue(value + 1)}
         >
           <svg
             className="w-6 h-6"
