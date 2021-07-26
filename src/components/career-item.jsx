aec823170bb2b397e67b63b620a7d5a2edd92d95
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-26 19:58:19 +0800

    create IconButton to extract shared code between EditButton and DeleteButton

--- src/components/career-item.jsx
+++ src/components/career-item.jsx
@@ -1,46 +1,55 @@
 import * as React from "react";
 
+const IconButton = (props) => (
+  <button
+    type="button"
+    className="p-1 rounded-full hover:bg-gray-50 focus:outline-none focus:bg-gray-50 focus:ring focus:ring-pink-500 focus:ring-opacity-30 transition duration-150 ease-in-out"
+    title={props.title}
+    onClick={props.onClick}
+  >
+    {props.icon}
+  </button>
+);
+
 const EditButton = (props) => {
   return (
-    <button
-      type="button"
-      className="js-edit-btn p-1 rounded-full hover:bg-gray-50 focus:outline-none focus:bg-gray-50 focus:ring focus:ring-pink-500 focus:ring-opacity-30 transition duration-150 ease-in-out"
+    <IconButton
       title="Edit"
       onClick={props.onClick}
-    >
-      <svg
-        className="h-5 w-5 text-gray-400"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-      </svg>
-    </button>
+      icon={
+        <svg
+          className="h-5 w-5 text-gray-400"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+        </svg>
+      }
+    />
   );
 };
 
 const DeleteButton = (props) => {
   return (
-    <button
-      type="button"
-      className="js-delete-btn p-1 rounded-full hover:bg-gray-50 focus:outline-none focus:bg-gray-50 focus:ring focus:ring-pink-500 focus:ring-opacity-30 transition duration-150 ease-in-out"
+    <IconButton
       title="Delete"
       onClick={props.onClick}
-    >
-      <svg
-        className="w-5 h-5 text-gray-400"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-    </button>
+      icon={
+        <svg
+          className="w-5 h-5 text-gray-400"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      }
+    />
   );
 };
 
