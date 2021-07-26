4b9bcc08154c46399b351950c4d9835ae358668f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-26 21:51:14 +0800

    add state to accordion

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 
 export const Accordion = ({ title, children }) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
   return (
     <div className="pt-6">
       <div className="text-lg">
         <button
           type="button"
           className="flex w-full items-center justify-between text-gray-400 focus:outline-none focus:ring focus:ring-offset-4"
+          onClick={() => setIsExpanded(!isExpanded)}
         >
           <span className="font-medium text-gray-900">{title}</span>
           <span className="ml-6 h-7 flex items-center">
             <svg
-              className="rotate-0 h-6 w-6 transform"
+              className={`h-6 w-6 transform ${
+                isExpanded ? "rotate-180" : "rotate-0"
+              }`}
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -28,7 +33,7 @@ export const Accordion = ({ title, children }) => {
           </span>
         </button>
       </div>
-      <div className="mt-2 pr-12">{children}</div>
+      {isExpanded && <div className="mt-2 pr-12">{children}</div>}
     </div>
   );
 };
