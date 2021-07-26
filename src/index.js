a1985df5e7d1d2042f8e8f12060b14728468e1fc
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-26 11:17:48 +0800

    add ListingItem

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-import { CareerItem } from "./components/career-item";
+// import { CareerItem } from "./components/career-item";
+import { ListingItem } from "./components/listing-item";
 
 ReactDOM.render(
   <div className="max-w-xl mx-auto p-6">
-    <CareerItem />
+    <ListingItem />
   </div>,
   document.querySelector("#root")
 );
