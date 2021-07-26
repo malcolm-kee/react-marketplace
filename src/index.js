2de9ce4e00238cd7b6d6fd9ebded910533ebdbc3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-26 12:10:32 +0800

    add props to components to make them reusable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import { CareerItem } from "./components/career-item";
 
 ReactDOM.render(
   <div className="max-w-xl mx-auto p-6">
-    <CareerItem />
+    <CareerItem
+      title="Memer"
+      department="UI / UX Design"
+      level="Experienced"
+      onEdit={() => alert("Edit btn clicked, populate the form!")}
+      onDelete={() => alert("Delete btn clicked, delete the item!")}
+    />
   </div>,
   document.querySelector("#root")
 );
