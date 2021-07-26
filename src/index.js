504f039ddd9d0eb0278ed10b5bd37875bbc5f853
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-26 12:05:28 +0800

    splitting components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-// import { CareerItem } from "./components/career-item";
-import { ListingItem } from "./components/listing-item";
+import { CareerItem } from "./components/career-item";
+// import { ListingItem } from "./components/listing-item";
 
 ReactDOM.render(
   <div className="max-w-xl mx-auto p-6">
-    <ListingItem />
+    <CareerItem />
   </div>,
   document.querySelector("#root")
 );
