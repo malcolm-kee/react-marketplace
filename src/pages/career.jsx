a6ca6f4c18fc7a016d5e21f17c5144de3a608057
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-27 16:35:24 +0800

    load jobs from api

--- src/pages/career.jsx
+++ src/pages/career.jsx
@@ -172,26 +172,37 @@ const jobs = [
 
 export const Career = () => {
   const [isLoading, setIsLoading] = React.useState(false);
+  const [jobs, setJobs] = React.useState(undefined);
 
   return (
     <div>
       <div className="max-w-xl mx-auto p-6 space-y-5">
-        {jobs.map((job) => (
-          <CareerItem
-            title={job.title}
-            department={job.department}
-            level={job.level}
-            onEdit={() => alert("Edit btn clicked, populate the form!")}
-            onDelete={() => alert("Delete btn clicked, delete the item!")}
-            key={job._id}
-          />
-        ))}
+        {jobs &&
+          jobs.map((job) => (
+            <CareerItem
+              title={job.title}
+              department={job.department}
+              level={job.level}
+              onEdit={() => alert("Edit btn clicked, populate the form!")}
+              onDelete={() => alert("Delete btn clicked, delete the item!")}
+              key={job._id}
+            />
+          ))}
       </div>
-      <div className="text-center">
-        <button onClick={() => setIsLoading(true)}>
-          {isLoading ? "Loading..." : "Load"}
-        </button>
-      </div>
+      {!jobs && (
+        <div className="text-center">
+          <button
+            onClick={() => {
+              setIsLoading(true);
+              fetch("https://ecomm-service.herokuapp.com/job?limit=5")
+                .then((res) => res.json())
+                .then((data) => setJobs(data));
+            }}
+          >
+            {isLoading ? "Loading..." : "Load"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
