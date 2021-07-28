41c5f99d134b054ab42cfd25fcceae7241e76eea
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-29 04:41:23 +0800

    use useEffect cleanup function to fix race condition

--- src/pages/career.jsx
+++ src/pages/career.jsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { CareerItem } from "../components/career-item";
 
-const getJobs = (page) => {
+const getJobs = (page, signal) => {
   return fetch(
     `https://ecomm-service.herokuapp.com/job?limit=5&page=${page}` +
-      (page === 2 ? "&delay=3000" : "")
+      (page === 2 ? "&delay=3000" : ""),
+    {
+      signal,
+    }
   ).then((res) => res.json());
 };
 
@@ -40,12 +43,17 @@ export const Career = () => {
 
   const titleInputRef = React.useRef();
 
-  const loadJobs = (pageNum) => getJobs(pageNum).then((data) => setJobs(data));
+  const loadJobs = (pageNum, signal) =>
+    getJobs(pageNum, signal).then((data) => setJobs(data));
 
   const [page, setPage] = React.useState(1);
 
   React.useEffect(() => {
-    loadJobs(page);
+    const ab = new AbortController();
+    loadJobs(page, ab.signal);
+    return () => {
+      ab.abort();
+    };
   }, [page]);
 
   return (
