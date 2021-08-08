f3c80bb774681084e9c78deda5d5524bc284ad38
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-08 14:59:46 +0800

    configure tailwindcss based on tailwindcss.com/docs/guides/create-react-app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./index.css";
 import { Marketplace } from "./pages/marketplace";
 
 ReactDOM.render(<Marketplace />, document.querySelector("#root"));
