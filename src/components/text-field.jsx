e76325ec5e1e7a9631924c6f8428c6fca0384003
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-09 21:14:53 +0800

    wrap TextField with forwardRef

--- src/components/text-field.jsx
+++ src/components/text-field.jsx
@@ -1,5 +1,6 @@
 import { useId } from "hooks/use-id";
 import PropTypes from "prop-types";
+import * as React from "react";
 import { Label } from "./label";
 import { TextInput } from "./text-input";
 
@@ -7,17 +8,22 @@ import { TextInput } from "./text-input";
  * `<TextField />` composed of `<Label />` and `<TextInput />` component.
  *
  * Unspecified props will be forwarded to the underlying `<TextInput />` element.
+ *
+ * `ref` will be forwarded to `<TextInput />` element.
  */
-export const TextField = ({ label, id, ...inputProps }) => {
+export const TextField = React.forwardRef(function TextField(
+  { label, id, ...inputProps },
+  ref
+) {
   const ensuredId = useId(id);
 
   return (
     <div className="space-y-1">
       {label && <Label htmlFor={ensuredId}>{label}</Label>}
-      <TextInput {...inputProps} id={ensuredId} />
+      <TextInput {...inputProps} id={ensuredId} ref={ref} />
     </div>
   );
-};
+});
 
 TextField.propTypes = {
   label: PropTypes.node,
