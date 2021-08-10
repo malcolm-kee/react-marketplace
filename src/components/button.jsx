aab33ffbad7a5242d7b9c9ca8bfe25991b8087b1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-10 13:37:34 +0800

    Button component support render props

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,17 +1,22 @@
 import cn from "classnames";
 import PropTypes from "prop-types";
 
-export const Button = ({ type = "button", variant, ...props }) => (
-  <button
-    type={type}
-    {...props}
-    className={cn(
-      "inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500",
-      variant && classByVariant[variant],
-      props.className
-    )}
-  />
-);
+export const Button = ({ type = "button", variant, render, ...props }) => {
+  const className = cn(
+    "inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500",
+    variant && classByVariant[variant],
+    props.className
+  );
+
+  if (render) {
+    return render({
+      className,
+      ...props,
+    });
+  }
+
+  return <button type={type} {...props} className={className} />;
+};
 
 const classByVariant = {
   primary: "border-transparent text-white bg-pink-600 hover:bg-pink-700",
@@ -20,4 +25,5 @@ const classByVariant = {
 
 Button.propTypes = {
   variant: PropTypes.oneOf(["primary", "outline"]),
+  render: PropTypes.func,
 };
--- src/components/button.md
+++ src/components/button.md
@@ -5,3 +5,14 @@
   <Button variant="outline">Outline</Button>
 </div>
 ```
+
+Use render to render element other than `button`, e.g. `<a>` tag.
+
+```jsx
+<Button
+  variant="primary"
+  render={(btnProps) => <a {...btnProps} href="https://google.com" />}
+>
+  Go to Google
+</Button>
+```
